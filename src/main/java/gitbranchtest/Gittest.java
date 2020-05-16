5721c5da8fabe00912a59ae45a65f3f680668a2b
17600267646 <aqw121346>
2020-05-16 15:43:41 +0800

    git测试2

--- src/main/java/gitbranchtest/Gittest.java
+++ src/main/java/gitbranchtest/Gittest.java
@@ -3,5 +3,7 @@ package gitbranchtest;
 public class Gittest {
     public static void main(String[] args) {
         System.out.println("gittest");
+        System.out.println("gittest2");
+
     }
 }
