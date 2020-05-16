f5836dfe90aacb24bf63b87a33cc92ae581fa210
17600267646 <aqw121346>
2020-05-16 16:40:17 +0800

    删除所有

--- src/main/java/gitbranchtest/Gittest.java
+++ src/main/java/gitbranchtest/Gittest.java
@@ -2,15 +2,6 @@ package gitbranchtest;
 
 public class Gittest {
     public static void main(String[] args) {
-        System.out.println("gittest");
-        System.out.println("gittest2");
-        System.out.println("gittest3");
-        System.out.println("gittest4");
-        System.out.println("gittest6");
-        System.out.println("gittest7");
-        System.out.println("gittest8gittest8");
-        System.out.println("gittest9");
-        System.out.println("gittest10");
 
 
 
