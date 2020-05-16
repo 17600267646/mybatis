52936be8189d32307d4e9edd03dbce0e25d17bb1
17600267646 <aqw121346>
2020-05-16 15:46:14 +0800

    gitttest测试3

--- src/main/java/gitbranchtest/Gittest.java
+++ src/main/java/gitbranchtest/Gittest.java
@@ -4,6 +4,8 @@ public class Gittest {
     public static void main(String[] args) {
         System.out.println("gittest");
         System.out.println("gittest2");
+        System.out.println("gittest3");
+
 
     }
 }
