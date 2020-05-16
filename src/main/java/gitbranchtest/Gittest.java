ceda630d13702b8d0542b6c9bb83b2fa16d26f16
17600267646 <aqw121346>
2020-05-16 18:12:56 +0800

    提交到本地本地仓库后revert

--- src/main/java/gitbranchtest/Gittest.java
+++ src/main/java/gitbranchtest/Gittest.java
@@ -3,5 +3,7 @@ package gitbranchtest;
 public class Gittest {
     public static void main(String[] args) {
         System.out.println("master测试");
+        System.out.println("提交到本地本地仓库后revert");
+
     }
 }
