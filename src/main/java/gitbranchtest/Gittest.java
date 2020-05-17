29137397cab5541d9587405488281b895c363cbc
17600267646 <aqw121346>
2020-05-17 12:54:30 +0800

    masterdev 测试dev提交

--- src/main/java/gitbranchtest/Gittest.java
+++ src/main/java/gitbranchtest/Gittest.java
@@ -3,5 +3,7 @@ package gitbranchtest;
 public class Gittest {
     public static void main(String[] args) {
         System.out.println("merge");
+        System.out.println("masterdev测试dev提交");
+
     }
 }
