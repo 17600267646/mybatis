7d6f84c44f3973c300ee49ae2d1570f48f7d21ac
17600267646 <aqw121346>
2020-05-17 13:32:06 +0800

    master

--- src/main/java/gitbranchtest/Gittest.java
+++ src/main/java/gitbranchtest/Gittest.java
@@ -2,15 +2,6 @@ package gitbranchtest;
 
 public class Gittest {
     public static void main(String[] args) {
-        System.out.println("merge");
-        System.out.println("masterdev测试dev提交");
-        System.out.println("master测试");
-        System.out.println("提交到本地本地仓库后revert");
-
-        System.out.println("1git ");
-
-        System.out.println("3git ");
-
-        System.out.println("masterdev测试 master提交");
+        System.out.println("master");
     }
 }
